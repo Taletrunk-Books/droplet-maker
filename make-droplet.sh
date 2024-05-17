c7a5d77bb946b8ece583c38e0594f7bd616c7871
Oğuzhan Kandakoğlu <oguzhankandakoglu@gmail.com>
2024-05-17 12:52:35 +0300

    chore: Update make-droplet.sh filename in setup instructions

--- make-droplet.sh
+++ make-droplet.sh
@@ -17,7 +17,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Docker repository
 echo "Adding Docker repository..."
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update package index again
 echo "Updating package index again..."
